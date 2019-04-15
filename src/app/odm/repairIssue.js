a4cdb510b60feeeae44f30c76c069cde754d83d1
TwoFistedJustice <phaltak-1@yahoo.com>
2019-04-15 11:38:26 -0700

    Add repair types to ODM

---
+++ src/app/odm/repairIssue.js
@@ -0,0 +1,25 @@
+// a repair issue is a known problem with the vehicle.
+
+
+// Description: briefly, what do you think is wrong? -string
+// Symptoms: What is happening that should not be, or is not that should be? - string
+
+// System affected: In which system does the problem originate - string
+// Part suspected: What part do you think is causing the issue - string
+
+/// What will fix the problem permanently - string
+
+// Are there other repairs that are necessary or convenient to do along the way - boolean
+  // Repair series: object ID of the repair series this doc belongs to.
+  // If none is chosen, one is created by default. Can be changed later.
+  
+
+// What parts are needed?
+
+// What special tools are needed?
+
+// completed: bool def- false
+
+// odometer when discovered
+// date discovered: def-date now
+
---
+++ src/app/odm/repairSeries.js
@@ -0,0 +1,16 @@
+// A repair series is a queue of repair issues. The repair issues should be in the order they need
+// to be completed in.
+
+
+// Steps/repairs needed - array or queue of objects
+  // obj id of repairIssue
+  // which step (order) integer 1-whatever
+
+
+// completed?
+
+//date completed
+
+// odo reading on completion
+
+
