20fced813995be7b050b4bb168ceb16c3b3a1859
TwoFistedJustice <phaltak-1@yahoo.com>
2019-05-13 13:21:56 -0700

    Add Inspection Point stub

---
+++ src/app/odm/inspectionPoint.js
@@ -0,0 +1,15 @@
+/* An Inspection Point recoreds that a User has inspected a given
+*  part or system of a Vehicle and determined its condition.
+*
+*  It should record what System/Part was inspected
+*  It should record the date of the inspection
+*  It should record the odometer reading as of the inspection
+*  It should record the conclusion reached upon inspection as a note entered by the user
+*  It should allow the User to estimate remaining life (if applicable, tires and such)
+*  It should allow the User to simply record that the part needs to be replaced
+*  It should be able to automatically create a Repair Issue if the inspection finds fault.
+*  It should allow the User to suppress the Repair Issue creation
+*  It should attach itself to any Repair issue it creates
+*  It should allow a User to attach it a given Repair Issue
+*
+* */
